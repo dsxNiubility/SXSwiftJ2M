710e22d6d354edaef538c6d725a0c3048cfc73a3
董铂然 <dantesx2012@gmail.com>
2015-03-04 13:53:13 +0800

    第一次上传
    文件初始化建立

---
+++ 105 - SXSwiftJ2M/105 - SXSwiftJ2M/SXSwiftJ2M.swift
@@ -0,0 +1,9 @@
+//
+//  SXSwiftJ2M.swift
+//  105 - SXSwiftJ2M
+//
+//  Created by 董 尚先 on 15/3/4.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+import Foundation
---
+++ 105 - SXSwiftJ2M/105 - SXSwiftJ2M/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  105 - SXSwiftJ2M
+//
+//  Created by 董 尚先 on 15/3/4.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
